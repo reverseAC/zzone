1f503744a547dcf594b5d5b59d843e2635138da7
zjh <jzhong529@163.com>
2025-07-18 17:30:19 +0800

    网关模块开发

---
+++ zzone-gateway/src/main/java/com/zjh/zzone/gateway/ZzoneGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.zjh.zzone.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务启动类
+ *
+ * @author zjh
+ * @since 2025-07-18 10:17
+ */
+@SpringBootApplication
+public class ZzoneGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZzoneGatewayApplication.class, args);
+    }
+}
---
+++ zzone-gateway/src/main/java/com/zjh/zzone/gateway/config/GatewayConfig.java
@@ -0,0 +1,15 @@
+package com.zjh.zzone.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 网关配置类
+ *
+ * @author zjh
+ * @date 2025-07-18 16:22
+ */
+@Configuration
+public class GatewayConfig {
+
+
+}
